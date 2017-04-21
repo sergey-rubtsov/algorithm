e6ad8ae9050c95b378f1b302b6dc78dc62fd511f
sergei.rubtcov <sergei.rubtcov@accenture.com>
2017-04-22 01:11:10 +0300

    add 3 new solutions

---
+++ src/main/java/my/algorithm/schedule/Task.java
@@ -0,0 +1,75 @@
+package my.algorithm.schedule;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Serg on 13.06.2016.
+ */
+public class Task {
+
+    public static void main(String[] args) {
+        //String test = "a0Ba";
+        //System.out.print(new Task().solution(test));
+        int a[] = {1, 3, 2, 1};
+        int b[] = {2, 4, 5, 3, 2};
+        System.out.print(new Task().solution(a, b));
+    }
+
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        LinkedList<Integer> listA = new LinkedList<>();
+        LinkedList<Integer> listB = new LinkedList<>();
+        for (int i = 0; i < A.length; i++){
+            listA.add(new Integer(A[i]));
+        }
+        for (int i = 0; i < B.length; i++){
+            listB.add(new Integer(B[i]));
+        }
+        Integer minA = listA.peek();
+        Integer minB = listB.peek();
+        while (!listA.isEmpty() && !listB.isEmpty()) {
+            if (minA.equals(minB)) {
+                return minA;
+            } else if (minA < minB) {
+                listA.poll();
+                minA = listA.peek();
+            } else if (minA > minB) {
+                listB.poll();
+                minB = listB.peek();
+            }
+        }
+        return -1;
+    }
+
+    public int solution(String S) {
+        List<String> subs = new ArrayList<>();
+        char[] symbols = S.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < symbols.length; i++) {
+            char ch = symbols[i];
+            if ((ch >= '0' && ch <= '9') || i == (symbols.length - 1)) {
+                if (i == (symbols.length - 1)) {
+                    builder.append(ch);
+                }
+                subs.add(builder.toString());
+                builder = new StringBuilder();
+            } else {
+                builder.append(ch);
+            }
+        }
+        int number = 0;
+        for (String sub : subs) {
+            if (!(sub.equals(sub.toLowerCase())) && sub.length() > 1) {
+                if (sub.length() > number) number = sub.length();
+            }
+        }
+        if (number == 0) return -1;
+        return number;
+    }
+
+
+}
---
+++ src/main/java/my/algorithm/schedule/TwoDArray.java
@@ -0,0 +1,58 @@
+package my.algorithm.schedule;
+
+import java.util.Scanner;
+
+/**
+ * Created by Serg on 06.02.2017.
+ * How to rotate 2D array
+ */
+public class TwoDArray {
+
+/*    3 5
+            13 4 8 14 1
+            9 6 3 7 21
+            5 12 17 9 3*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split(" ");
+        int N = Integer.parseInt(arr[0]);
+        int M = Integer.parseInt(arr[1]);
+        int a[][] = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String line = scanner.nextLine();
+            arr = line.split(" ");
+            for (int j = 0; j < M; j++) {
+                a[i][j] = Integer.parseInt(arr[j]);
+            }
+        }
+        a = rotate(a);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    protected static int[][] rotate(int[][] input) {
+        int[][] rotated = new int[input[0].length][input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                rotated[j][i] = input[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    protected static int[][] mirror(int[][] input) {
+        int[][] result = new int[input[0].length][input.length];
+        for (int i = 0, k = result.length - 1; i < result.length; i++, k--) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = input[k][j];
+            }
+        }
+        return result;
+    }
+
+}
